774b8732a281792ed14215d3c10cc27fe071c6ee
SolidIcepeak <113999599+SolidIcepeak@users.noreply.github.com>
2022-10-19 22:08:57 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        int Balance = -789; //Текущий баланс
-        int BalanceRefill = 1489; //Сумма пополнения баланса
-        int Bonus = 100; //Количество рублей, за которые выдается бонусный рубль сверх пополнения баланса
-        int BalanceTotal = 0; //Итоговый Баланс
-        if (BalanceRefill > 1000) {
-            BalanceTotal = Balance + BalanceRefill + (BalanceRefill / Bonus);
+        int balance = -789; //Текущий баланс
+        int balancerefill = 1489; //Сумма пополнения баланса
+        int bonus = 100; //Количество рублей, за которые выдается бонусный рубль сверх пополнения баланса
+        int balancetotal = 0; //Итоговый Баланс
+        if (balancerefill > 1000) {
+            balancetotal = balance + balancerefill + (balancerefill / bonus);
         } else {
-            BalanceTotal = Balance + BalanceRefill;
+            balancetotal = balance + balancerefill;
         } //Рассчет итогового баланса с пополнением и бонусными рублями
-        System.out.println(BalanceTotal); //Вывод итогового баланса с пополнением и бонусными рублями
+        System.out.println(balancetotal); //Вывод итогового баланса с пополнением и бонусными рублями
     }
 }
